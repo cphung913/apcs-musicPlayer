f3aa610289cf06b0fd57fc669d7919011ba4c504
Chase Phung <69651488+cphung913@users.noreply.github.com>
2024-05-28 10:28:18 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+const musicContainer = document.getElementById('music-container');
+const title = document.getElementById('title');
+const audio = document.getElementById('audio');
+const cover = document.getElementById('cover');
+const playBtn = document.getElementById('play');
 
+const songs = ['hey', 'summer', 'ukulele'];
+
+let songIndex = 0;
+
+loadSong(songs[songIndex]);
+
+function loadSong(song) {
+    title.innerText = song;
+    audio.src = `music/${song}.mp3`;
+    cover.src = `images/${song}.jpg`;
+}
+
+playBtn.addEventListener('click', () => {
+    musicContainer.classList.toggle('play');
+});
